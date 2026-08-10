d35d1ef5223f0558f92353c8502ede9997df9362
Tarlis Portela <tarlis@tarlis.com.br>
2026-08-10 13:13:14 -0300

    Resultados PR

--- run_t2.sh
+++ run_t2.sh
@@ -1,6 +1,6 @@
-./run_pipeline.sh telemacoborba
-./run_pipeline.sh terraboa
-./run_pipeline.sh terrarica
+#./run_pipeline.sh telemacoborba
+#./run_pipeline.sh terraboa
+#./run_pipeline.sh terrarica
 ./run_pipeline.sh terraroxa
 
 ./run_pipeline.sh saomigueldoiguacu
